0398625840744e6f95b092fa69878caf65fda9d1
mannycolon <colonmanuel7@gmail.com>
2018-07-21 16:22:14 -0400

    Added build and start scripts to deploy on now

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,64 @@
 import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 import Layout from '../components/MyLayout.js'
 
-// as={`/p/${props.id}`} is used for masking the url
-const PostLink = (props) => (
+const PostLink = ({ show }) => (
   <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+      <a>{show.name}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 )
 
-const Index = () => (
+// as={`/p/${props.id}`} is used for masking the url
+const Index = (props) => (
   <Layout>
-    <h1>My Blog</h1>
+    <h1>Superman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js" />
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+      {props.shows.map(({show}) => <PostLink key={show.id} show={show}/>)}
     </ul>
+     <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+      ul {
+        padding: 0;
+      }
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 )
 
+Index.getInitialProps = async function () {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=superman')
+  const data = await res.json()
+  console.log(`Show data fetched. Count: ${data.length}`)
+  return {
+    shows: data
+  }
+}
 export default Index
--- pages/post.js
+++ pages/post.js
@@ -1,18 +1,48 @@
-import {withRouter} from 'next/router'
-import MyLayout from '../components/MyLayout'
+import fetch from 'isomorphic-unfetch'
+// import Markdown from 'react-markdown'
 import Layout from '../components/MyLayout';
 
-const Content = withRouter((props) => (
-  <div>
-    <h1>{props.router.query.title}</h1>
-    <p>This is the blog post content.</p>
-  </div>
-))
-
-const Page = (props) => (
+const Post = (props) => (
   <Layout>
-    <Content />
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium}/>
+    {/* EXAMPLE OF global style rules with styled-jsx
+    <div className="markdown">
+     <Markdown source={`
+This is our blog post.
+Yes. We can have a [link](/link).
+And we can have a title as well.
+### This is a title
+And here's the content.
+     `}/>
+   </div>
+   <style jsx global>{`
+     .markdown {
+       font-family: 'Arial';
+     }
+     .markdown a {
+       text-decoration: none;
+       color: blue;
+     }
+     .markdown a:hover {
+       opacity: 0.6;
+     }
+     .markdown h3 {
+       margin: 0;
+       padding: 0;
+       text-transform: uppercase;
+     }
+  `}</style> */}
   </Layout>
 )
 
-export default Page
+Post.getInitialProps = async function (context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
+  console.log(`Fetched show: ${show.name}`)
+  return { show }
+}
+
+export default Post
